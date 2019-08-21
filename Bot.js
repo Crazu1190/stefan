112038ebbdb0408229e368b5272937aaf3c2ddc1
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-21 15:06:03 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -10,7 +10,7 @@ client.on('message', message => {
         message.reply('Cześć ta komenda jest jeszcze nie skończona!');
   	}
     if (message.content === '!info');
-        messageml.channel.send('Cześć jestem **Stefan** jestem botem zostałem stworzony dla tego serwera przez Crazy1190 pamiętaj , że jestem w wersji 1.0 **Beta** i mogę mieć dużo błędów pomoge ci po komendą **!pomoc**');
+        message.channel.send('Cześć jestem **Stefan** jestem botem zostałem stworzony dla tego serwera przez Crazy1190 pamiętaj , że jestem w wersji 1.0 **Beta** i mogę mieć dużo błędów pomoge ci po komendą **!pomoc**');
         }
 });
 

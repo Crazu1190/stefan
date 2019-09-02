2eec96e0faa253bcf563132a2e26f453a377a3bd
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-09-02 13:20:56 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,26 +1,27 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 
-client.on('ready', () => { 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('Stefan', {type: 'WATCHING'});
+  });
+  
+  client.on('message', msg => {
+    if (msg.content === '!mem') {
+      msg.channel.send('```Konkurs na mem miesiąca się jeszcze nie skończył! Konkurs skończy się dnia  10.09.2019r```');
+    }
 
-});
 
-client.on('message', message => {
-    if (message.content === '!pomoc') {
-    	message.channel.send(' ');
-        message.channel.send('Ups...');
-        message.channel.send('Ta komenda jest jeszcze nie skończona!');
-        message.channel.send(' ');
-  	}
-    if (message.content === '!info') {
-        message.channel.send('■■■■■■■■■■■■');
-        message.channel.send('Czesc jestem Stefan');
-        message.channel.send('zostałem stworzony przez Crazy1190');
-        message.channel.send('Pisze w języku polskim');
-        message.channel.send('Jestem w wersji 1.0 Beta');
-        message.channel.send('■■■■■■■■■■■■');
-        }
-});
+    if (msg.content.includes('https://discord.gg/')) { 
+      
+      msg.author.sendMessage("Zgodnie z regulaminem zakazuję się reklamowania serwerów poza kanałami <#610974847165661200>, <#610974781055172608> i <#610974847165661200>  po uzgodnieniu tego z Administratorem");
+      msg.delete(10);
+    }
+    
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
+  });
+  
+
+client.login(config.token);
